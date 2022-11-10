1ca810bf23522fcd8e010f0f456739b07724f109
Josu Gomez Arana <josugoar@gmail.com>
2022-11-10 12:52:31 +0100

    Rename eAuctions and eAution to Strava

---
+++ Strava_Server_v1/src/es/deusto/ingenieria/sd/strava/server/data/dto/AthleteDTO.java
@@ -0,0 +1,14 @@
+package es.deusto.ingenieria.sd.strava.server.data.dto;
+
+import java.io.Serializable;
+
+//This class implements DTO pattern
+public class AthleteDTO implements Serializable {
+	//This attribute is needed to implement the "Serializable" interface.
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String toString() {
+		return null;
+	}
+}
