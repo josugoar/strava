cc5f5d09dac43152cc5f60ad71c934256f65edd5
Josu Gomez Arana <josugoar@gmail.com>
2022-11-10 12:53:02 +0100

    Rename eAuctions and eAuctions to Strava

--- Strava_Server_v1/src/es/deusto/ingenieria/sd/auctions/server/MainProgram.java
+++ Strava_Server_v1/src/es/deusto/ingenieria/sd/auctions/server/MainProgram.java
@@ -7,7 +7,7 @@ import es.deusto.ingenieria.sd.auctions.server.remote.RemoteFacade;
 
 public class MainProgram {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		//Activate Security Manager. It is needed for RMI.
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new SecurityManager());
@@ -16,15 +16,15 @@ public class MainProgram {
 		//args[0] = RMIRegistry IP
 		//args[1] = RMIRegistry Port
 		//args[2] = Service Name
-		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];		
-		
+		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
+
 		//Bind remote facade instance to a sirvice name using RMIRegistry
 		try {
-			IRemoteFacade remoteFacade = new RemoteFacade();			
+			IRemoteFacade remoteFacade = new RemoteFacade();
 			Naming.rebind(name, remoteFacade);
-			System.out.println(" * eAuction Server v1 '" + name + "' started!!");
+			System.out.println(" * Strava Server v1 '" + name + "' started!!");
 		} catch (Exception ex) {
-			System.err.println(" # eAuction Server Exception: " + ex.getMessage());
+			System.err.println(" # Strava Server Exception: " + ex.getMessage());
 			ex.printStackTrace();
 		}
 	}
