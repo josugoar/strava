dc1e52ae9328455e2bfac9998e23fec4f68d9e61
Josu Gomez Arana <josugoar@gmail.com>
2022-11-12 14:29:07 +0100

    Add couple tests

--- Strava_Server_v1/src/es/deusto/ingenieria/sd/strava/server/test/LocalTest.java
+++ Strava_Server_v1/src/es/deusto/ingenieria/sd/strava/server/test/LocalTest.java
@@ -1,5 +1,11 @@
 package es.deusto.ingenieria.sd.strava.server.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import es.deusto.ingenieria.sd.strava.server.data.dto.ChallengeDTO;
 import es.deusto.ingenieria.sd.strava.server.remote.RemoteFacade;
 
 // TODO
@@ -8,13 +14,33 @@ public class LocalTest {
 	public static void main(String[] args) {
 		RemoteFacade facade = null;
 		long token = 0l;
+		String pasword = org.apache.commons.codec.digest.DigestUtils.sha1Hex("$!9PhNz,");
+
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy", Locale.ENGLISH);
 
 		try {
+			// Login (fails no registered athlete)
+			facade.login("Peter Oben", pasword);
 		} catch (Exception e) {
 			System.out.println("\t# Error: " + e.getMessage());
 		}
 
-		//Force exit to stop RMI Server
+		try {
+			token = facade.register("peter.oben@gmail.com", "Peter Oben", pasword, new Date(), null, null, null, null);
+			ChallengeDTO newChallenge = facade.createChallenge(token, "hike", formatter.parse("22-01-2022"), formatter.parse("23-01-2029"), 10,
+					null, true, false);
+			List<ChallengeDTO> challenges = facade.getActiveChallenges(token);
+			for (ChallengeDTO challenge : challenges) {
+				System.out.println(challenge);
+			}
+			facade.logout(token);
+			float challengeState = facade.getChallengeState(newChallenge.getId());
+			System.out.println(challengeState);
+		} catch (Exception e) {
+			System.out.println("\t# Error: " + e.getMessage());
+		}
+
+		// Force exit to stop RMI Server
 		System.exit(0);
 	}
 }
