b6fd15d2fbc5819c50cee09573ae9ee2714205cf
Josu Gomez Arana <josugoar@gmail.com>
2022-11-12 13:49:05 +0100

    Implement challenge services

--- Strava_Server_v1/src/es/deusto/ingenieria/sd/strava/server/services/ChallengeAppService.java
+++ Strava_Server_v1/src/es/deusto/ingenieria/sd/strava/server/services/ChallengeAppService.java
@@ -1,12 +1,18 @@
 package es.deusto.ingenieria.sd.strava.server.services;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import es.deusto.ingenieria.sd.strava.server.data.domain.Athlete;
 import es.deusto.ingenieria.sd.strava.server.data.domain.Challenge;
 
 public class ChallengeAppService {
+
+    private Map<Integer, Challenge> state = new HashMap<>();
 
     public List<Challenge> getActiveChallenges(Athlete athlete) {
         List<Challenge> challenges = new ArrayList<>();
@@ -19,16 +25,54 @@ public class ChallengeAppService {
 
     }
 
-    public void createChallenge(Athlete athlete, Challenge challenge) {
+    public boolean createChallenge(Athlete athlete, String name, Date startDate, Date endDate, float distance, Duration time,
+            boolean isRunning, boolean isCycling) {
+        Challenge challenge = new Challenge();
+        challenge.setCycling(isCycling);
+        challenge.setDistance(distance);
+        challenge.setEndDate(endDate);
+        challenge.setName(name);
+        challenge.setRunning(isRunning);
+        challenge.setStartDate(startDate);
+        challenge.setTime(time);
+
+        if (!challenge.checkChallenge()) {
+            return false;
+        }
+
+        state.put(state.size(), challenge);
+        athlete.addChallenge(challenge);
+
+        return true;
+    }
+
+    public void acceptChallenge(Athlete athlete, int challengeId) throws IllegalArgumentException {
+        Challenge challenge = state.get(challengeId);
+        if (challenge == null) {
+            throw new IllegalArgumentException("Challenge does not exist!");
+        }
+
+        if (athlete.getChallenges().contains(challenge)) {
+            throw new IllegalArgumentException("Athlete already in challenge!");
+        }
+
         athlete.addChallenge(challenge);
     }
 
-    public void acceptChallenge(Athlete athlete, Challenge challenge) {
-        athlete.addChallenge(challenge);
-    }
+    public float getChallengeState(int challengeId) throws IllegalArgumentException {
+        Challenge challenge = state.get(challengeId);
+        if (challenge == null) {
+            throw new IllegalArgumentException("Challenge does not exist!");
+        }
 
-    public static float getChallengeState(Challenge challenge) {
-        return 0;
+        Date currentDate = new Date();
+        int relation = currentDate.compareTo(challenge.getEndDate());
+
+        if (relation >= 0) {
+            return 1;
+        }
+
+        return (currentDate.getTime() - challenge.getStartDate().getTime()) / (challenge.getEndDate().getTime() - challenge.getStartDate().getTime());
     }
 
 }
