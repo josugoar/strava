517743e23e80749c24817109f9cfe335a51c3063
Josu Gomez Arana <josugoar@gmail.com>
2022-11-12 13:31:04 +0100

    Cleanup facade interface

--- Strava_Server_v1/src/es/deusto/ingenieria/sd/strava/server/remote/IRemoteFacade.java
+++ Strava_Server_v1/src/es/deusto/ingenieria/sd/strava/server/remote/IRemoteFacade.java
@@ -2,28 +2,35 @@ package es.deusto.ingenieria.sd.strava.server.remote;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.Duration;
 import java.util.Date;
 import java.util.List;
 
-import es.deusto.ingenieria.sd.strava.server.data.dto.ActivityDTO;
 import es.deusto.ingenieria.sd.strava.server.data.dto.ChallengeDTO;
 
 //This interface defines the API of the Server. It represents the Remote Facade pattern
 public interface IRemoteFacade extends Remote {
 
-	public long register(String email, String password, String name, Date birthDate) throws RemoteException;
+	public long register(String email, String password, String name, Date birthDate, float weight, int height,
+			int restingHeartrate, int maxHeartrate) throws RemoteException;
 
 	public long login(String email, String password) throws RemoteException;
 
-	public void logout(long token)  throws RemoteException;
+	public void logout(long token) throws RemoteException;
 
-	public void createActivity(long token, ActivityDTO activity) throws RemoteException;
+	public void createActivity(long token, String name, float distance, Duration elapsedTime, String type,
+			Date startDate) throws RemoteException;
 
-	public void createChallenge(long token, ChallengeDTO challenge) throws RemoteException;
+	public void createChallenge(long token, Date startDate,
+			Date endDate,
+			float distance,
+			Duration time,
+			boolean isCycling,
+			boolean isRunning) throws RemoteException;
 
 	public List<ChallengeDTO> getActiveChallenges(long token) throws RemoteException;
 
-	public void acceptChallenge(long token, ChallengeDTO challenge) throws RemoteException;
+	public void acceptChallenge(long token, int challenge) throws RemoteException;
 
-	public float getChallengeState(ChallengeDTO challenge) throws RemoteException;
+	public float getChallengeState(int challenge) throws RemoteException;
 }
