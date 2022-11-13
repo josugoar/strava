494ebc14e588cf3c1a4f97cd206809e3beac7f79
Josu Gomez Arana <josugoar@gmail.com>
2022-11-13 12:02:30 +0100

    Add ChallengeController methods

--- Strava_Client/src/es/deusto/ingenieria/sd/strava/client/controller/ChallengeController.java
+++ Strava_Client/src/es/deusto/ingenieria/sd/strava/client/controller/ChallengeController.java
@@ -1,6 +1,13 @@
 package es.deusto.ingenieria.sd.strava.client.controller;
 
+import java.rmi.RemoteException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import es.deusto.ingenieria.sd.strava.client.remote.ServiceLocator;
+import es.deusto.ingenieria.sd.strava.server.data.dto.ChallengeDTO;
 
 public class ChallengeController {
 
@@ -8,6 +15,48 @@ public class ChallengeController {
 
     public ChallengeController(ServiceLocator serviceLocator) {
         this.serviceLocator = serviceLocator;
+    }
+
+    public ChallengeDTO createChallenge(long token, String name, Date startDate,
+            Date endDate,
+            float distance,
+            Duration time,
+            boolean isCycling,
+            boolean isRunning) {
+        try {
+            return this.serviceLocator.getService().createChallenge(token, name, startDate, endDate, distance, time, isCycling, isRunning);
+        } catch (RemoteException e) {
+            System.out.println("# Error creating an activity: " + e);
+            return null;
+        }
+    }
+
+    public List<ChallengeDTO> getActiveChallenges(long token) {
+        try {
+            return this.serviceLocator.getService().getActiveChallenges(token);
+        } catch (RemoteException e) {
+            System.out.println("# Error creating an activity: " + e);
+            return new ArrayList<>();
+        }
+    }
+
+    public boolean acceptChallenge(long token, int challengeId) {
+        try {
+            this.serviceLocator.getService().acceptChallenge(token, challengeId);;
+            return true;
+        } catch (RemoteException e) {
+            System.out.println("# Error creating an activity: " + e);
+            return false;
+        }
+    }
+
+    public float getChallengeState(long token, int challengeId) {
+        try {
+            return this.serviceLocator.getService().getChallengeState(token, challengeId);
+        } catch (RemoteException e) {
+            System.out.println("# Error creating an activity: " + e);
+            return -1;
+        }
     }
 
 }
