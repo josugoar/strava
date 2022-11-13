a4cc3ba830e0e1cae5ec944d804601e492afe33d
Josu Gomez Arana <josugoar@gmail.com>
2022-11-13 19:29:25 +0100

    Delete unused imports

--- Strava_Client/src/es/deusto/ingenieria/sd/strava/client/gui/MainWindow.java
+++ Strava_Client/src/es/deusto/ingenieria/sd/strava/client/gui/MainWindow.java
@@ -6,8 +6,6 @@ import java.awt.event.ComponentEvent;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ComponentListener;
-import java.time.Duration;
 import java.util.List;
 
 import javax.swing.AbstractAction;
