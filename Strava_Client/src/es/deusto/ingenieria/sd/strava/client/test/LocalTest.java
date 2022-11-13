7a29b62958daa8005ad1b5afa5f5c83c6ca6923a
gaizka-basterra <gaizka.basterra@opendeusto.es>
2022-11-13 14:45:00 +0100

    challenge dialog

--- Strava_Client/src/es/deusto/ingenieria/sd/strava/client/test/LocalTest.java
+++ Strava_Client/src/es/deusto/ingenieria/sd/strava/client/test/LocalTest.java
@@ -3,6 +3,8 @@ package es.deusto.ingenieria.sd.strava.client.test;
 import javax.print.attribute.standard.ReferenceUriSchemesSupported;
 
 import es.deusto.ingenieria.sd.strava.client.controller.AthleteController;
+import es.deusto.ingenieria.sd.strava.client.controller.ChallengeController;
+import es.deusto.ingenieria.sd.strava.client.gui.ChallengeDialog;
 import es.deusto.ingenieria.sd.strava.client.gui.LoginWindow;
 import es.deusto.ingenieria.sd.strava.client.gui.RegisterWindow;
 import es.deusto.ingenieria.sd.strava.client.remote.ServiceLocator;
@@ -13,10 +15,13 @@ public class LocalTest {
     public static void main(String[] args) {
         ServiceLocator serviceLocator = new ServiceLocator();
         AthleteController athleteController = new AthleteController(serviceLocator);
+        ChallengeController challengeController = new ChallengeController(serviceLocator);
 
         LoginWindow loginWindow = new LoginWindow(athleteController);
 
         RegisterWindow registerWindow = new RegisterWindow(athleteController);
+
+        ChallengeDialog challengeDialog = new ChallengeDialog(challengeController);
         
 
 
