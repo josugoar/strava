0b2ca73f926afd3f7d407c6c4dab8586b7971ac2
Josu Gomez Arana <josugoar@gmail.com>
2022-11-13 11:53:44 +0100

    Implement AthleteController methods

--- Strava_Client/src/es/deusto/ingenieria/sd/strava/client/controller/AthleteController.java
+++ Strava_Client/src/es/deusto/ingenieria/sd/strava/client/controller/AthleteController.java
@@ -1,13 +1,61 @@
 package es.deusto.ingenieria.sd.strava.client.controller;
 
+import java.rmi.RemoteException;
+import java.util.Date;
+
 import es.deusto.ingenieria.sd.strava.client.remote.ServiceLocator;
+import es.deusto.ingenieria.sd.strava.server.data.dto.AthleteDTO;
 
 public class AthleteController {
 
     ServiceLocator serviceLocator;
+    private Long token;
 
     public AthleteController(ServiceLocator serviceLocator) {
         this.serviceLocator = serviceLocator;
     }
+
+    public boolean register(String email, String password, String name, Date birthDate, Float weight, Integer height,
+            Integer restingHeartrate, Integer maxHeartrate) {
+        try {
+            this.token = this.serviceLocator.getService().register(email, password, name, birthDate, weight, height, restingHeartrate, maxHeartrate);
+            return true;
+        } catch (RemoteException e) {
+            System.out.println("# Error during register: " + e);
+            return false;
+        }
+    }
+
+    public boolean login(String email, String password) {
+        try {
+            this.token = this.serviceLocator.getService().login(email, password);
+            return true;
+        } catch (RemoteException e) {
+            System.out.println("# Error during login: " + e);
+            return false;
+        }
+    }
+
+    public void logout() {
+        try {
+            this.serviceLocator.getService().logout(this.token);
+            this.token = null;
+        } catch (RemoteException e) {
+            System.out.println("# Error during logout: " + e);
+        }
+    }
+
+    public AthleteDTO getAthlete() {
+        try {
+            return this.serviceLocator.getService().getAthlete(this.token);
+        } catch (RemoteException e) {
+            System.out.println("# Error during logout: " + e);
+            return null;
+        }
+    }
+
+    public long getToken() {
+		return token;
+	}
 
 }
