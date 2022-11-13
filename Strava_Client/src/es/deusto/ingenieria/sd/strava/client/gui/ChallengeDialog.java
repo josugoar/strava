a0b13f972263c39e6f8b11b7850f0a464e06f5e4
Josu Gomez Arana <josugoar@gmail.com>
2022-11-13 19:49:47 +0100

    Fix broken exit dialog behaviour

--- Strava_Client/src/es/deusto/ingenieria/sd/strava/client/gui/ChallengeDialog.java
+++ Strava_Client/src/es/deusto/ingenieria/sd/strava/client/gui/ChallengeDialog.java
@@ -34,7 +34,7 @@ public class ChallengeDialog extends JDialog {
         initDialog();
         setContentPane(mainPane);
         setSize(400,400);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
     }
 
     public void initDialog() {
