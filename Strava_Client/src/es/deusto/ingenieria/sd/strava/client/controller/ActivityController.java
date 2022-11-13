76f1177ee4e7473c6eb468fe20247a826185782c
Josu Gomez Arana <josugoar@gmail.com>
2022-11-13 11:57:31 +0100

    Implement ActivityController methods

--- Strava_Client/src/es/deusto/ingenieria/sd/strava/client/controller/ActivityController.java
+++ Strava_Client/src/es/deusto/ingenieria/sd/strava/client/controller/ActivityController.java
@@ -1,6 +1,13 @@
 package es.deusto.ingenieria.sd.strava.client.controller;
 
+import java.rmi.RemoteException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import es.deusto.ingenieria.sd.strava.client.remote.ServiceLocator;
+import es.deusto.ingenieria.sd.strava.server.data.dto.ActivityDTO;
 
 public class ActivityController {
 
@@ -8,6 +15,25 @@ public class ActivityController {
 
     public ActivityController(ServiceLocator serviceLocator) {
         this.serviceLocator = serviceLocator;
+    }
+
+    public ActivityDTO createActivity(long token, String name, float distance, Duration elapsedTime, String type,
+            Date startDate) {
+        try {
+            return this.serviceLocator.getService().createActivity(token, name, distance, elapsedTime, type, startDate);
+        } catch (RemoteException e) {
+            System.out.println("# Error creating an activity: " + e);
+            return null;
+        }
+    }
+
+    public List<ActivityDTO> getActivities(long token) {
+        try {
+            return this.serviceLocator.getService().getActivities(token);
+        } catch (RemoteException e) {
+            System.out.println("# Error getting activities: " + e);
+            return new ArrayList<>();
+        }
     }
 
 }
