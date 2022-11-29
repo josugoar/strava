ba0d3518ae369edcb60a84deee673cfdf44da2bb
Josu Gomez Arana <josugoar@gmail.com>
2022-11-29 13:41:35 +0100

    Improve gateway factory using enum

--- Strava_Server_v2/src/es/deusto/ingenieria/sd/strava/server/factory/GatewayFactory.java
+++ Strava_Server_v2/src/es/deusto/ingenieria/sd/strava/server/factory/GatewayFactory.java
@@ -1,21 +1,22 @@
 package es.deusto.ingenieria.sd.strava.server.factory;
 
+import es.deusto.ingenieria.sd.strava.server.data.domain.LoginType;
 import es.deusto.ingenieria.sd.strava.server.gateway.FacebookServiceGateway;
 import es.deusto.ingenieria.sd.strava.server.gateway.GoogleServiceGateway;
 import es.deusto.ingenieria.sd.strava.server.gateway.IGateway;
 
 public class GatewayFactory {
 
-    public static IGateway createGateway(final String gateway) throws IllegalArgumentException {
-        switch (gateway) {
-            case "Facebook":
+    public static IGateway createGateway(final LoginType loginType) {
+        switch (loginType) {
+            case FACEBOOK:
                 return FacebookServiceGateway.getInstance();
 
-            case "Google":
+            case GOOGLE:
                 return GoogleServiceGateway.getInstance();
 
             default:
-                throw new IllegalArgumentException("Invalid gateway!");
+                throw null;
         }
     }
 
